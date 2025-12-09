25f62e9fa0888612444eb440195aad28c65ec902
Lianna Fung <lianna.fung@live.co.uk>
2025-12-09 18:45:23 +0000

    Update Balls.py

--- Balls.py
+++ Balls.py
@@ -1,4 +1,4 @@
-### Calculates the density of TiO2 balls
+### Calculates the mean free path of background
 
 import numpy as np
 
@@ -49,4 +49,5 @@ suspension_and_ball = abs_m_balls*1000/0.4  # in g
 water_for_agarose = 50-suspension_and_ball
 
 print(suspension_and_ball/5, water_for_agarose/5)
+
 
